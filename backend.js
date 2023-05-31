75acb6ef6e2cf1b28cc62aeb5f34c1b150d35f83
Obodugo Rapheal <kofacts@gmail.com>
2023-05-31 07:16:36 +0100

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const csv = require('csv-parser');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.get('/api/orders/:email', (req, res) => {
   const { email } = req.params;
   // Read and process the trackings.csv file
   const orders = [];
-  fs.createReadStream('data/trackings.csv')
+  fs.createReadStream(path.join(__dirname, 'data', 'trackings.csv'))
     .pipe(csv())
     .on('data', (data) => {
       if (data.email === email) {
@@ -25,6 +26,9 @@ app.get('/api/orders/:email', (req, res) => {
     })
     .on('end', () => {
       res.json(orders);
+    })
+    .on('error', (err) => {
+      res.status(500).json({ error: 'Failed to read order data' });
     });
 });
 
@@ -37,7 +41,7 @@ app.get('/api/orders/:orderId', (req, res) => {
     tracking_number: '',
     checkpoints: [],
   };
-  fs.createReadStream('data/trackings.csv')
+  fs.createReadStream(path.join(__dirname, 'data', 'trackings.csv'))
     .pipe(csv())
     .on('data', (data) => {
       if (data.orderNo === orderId) {
@@ -46,7 +50,7 @@ app.get('/api/orders/:orderId', (req, res) => {
         // Include other relevant properties
 
         // Read and process the checkpoints.csv file
-        fs.createReadStream('data/checkpoints.csv')
+        fs.createReadStream(path.join(__dirname, 'data', 'checkpoints.csv'))
           .pipe(csv())
           .on('data', (checkpointData) => {
             if (checkpointData.tracking_number === order.tracking_number) {
@@ -62,11 +66,17 @@ app.get('/api/orders/:orderId', (req, res) => {
           })
           .on('end', () => {
             res.json(order);
+          })
+          .on('error', (err) => {
+            res.status(500).json({ error: 'Failed to read checkpoint data' });
           });
       }
     })
     .on('end', () => {
       res.status(404).json({ error: 'Order not found' });
+    })
+    .on('error', (err) => {
+      res.status(500).json({ error: 'Failed to read order data' });
     });
 });
 
